bdb01c6070f0b53d6aaf737baa9dbcd29a42884f
Valentine <valentinelav98@gmail.com>
2019-11-01 20:22:02 +0100

    :sparkles: Redirect to wikipedia page

--- app/src/main/java/com/example/projetkotlin/game/GameDetailFragment.kt
+++ app/src/main/java/com/example/projetkotlin/game/GameDetailFragment.kt
@@ -1,11 +1,16 @@
 package com.example.projetkotlin.game
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.projetkotlin.HttpRequestActivity
 import com.example.projetkotlin.R
+import com.example.projetkotlin.WikiViewActivity
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_game_detail.*
 
@@ -22,6 +27,12 @@ class GameDetailFragment(private val game: Game) : Fragment() {
         Picasso.get().load(this.game.img).into(img)
         description.text = game.description
         name.text = game.name
+
+        link.setOnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse(game.link)
+            startActivity(openURL)
+        }
 
         super.onViewCreated(view, savedInstanceState)
     }
